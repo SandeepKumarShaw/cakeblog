b0ffed0763860e36506644676d5b5a9ad8b724af
sandeeptier5 <sandy.shaw20@gmail.com>
2020-03-01 21:48:00 +0530

    cake bake setup

--- app/Controller/TopicsController.php
+++ app/Controller/TopicsController.php
@@ -5,19 +5,27 @@
 */
 class TopicsController extends AppController
 {
-	public $components = array('Session');
+	//public $components = array('Session');
+	public $components = array('Flash');
 	public function index(){
+		$data = $this->Topic->find('all');
+		$this->set('topics', $data);
 
 	}	
 	public function add(){
 		if ($this->request->is('post')) {
 			$this->Topic->create();
 			if($this->Topic->save($this->request->data)){
-				$this->Session->setFlash('The topic has been created!');
+				//$this->Session->setFlash('The topic has been created!');
+				$this->Flash->set('The topic has been created'); 
 				$this->redirect('index');
 			}
 		}
 
+	}
+	public function view($id){
+		$data = $this->Topic->findById($id);
+		$this->set('topic', $data);
 	}
 }
 
--- app/View/Topics/index.ctp
+++ app/View/Topics/index.ctp
@@ -1,4 +1,25 @@
-<h1>Create Topics</h1>
+<h1>Topics</h1>
+<table>
+	<tr>
+		<td>Title</td>
+		<td>User ID</td>
+		<td>Publisher</td>
+		<td>Created</td>
+		<td>Modified</td>
+	</tr>
 <?php
-	echo "index.php";
+	foreach ($topics as $topic) 
+	{
+?>	
+		<tr>
+			<td><?php echo $this->HTML->link($topic['Topic']['title'], array('controller' => 'topics', 'action' => 'view', $topic['Topic']['id'])); ?></td>
+			<td><?php echo $topic['Topic']['user_id']; ?></td>
+			<td><?php echo $topic['Topic']['visible']; ?></td>
+			<td><?php echo $topic['Topic']['created']; ?></td>
+			<td><?php echo $topic['Topic']['modified']; ?></td>
+		</tr>
+<?php
+	}
+	unset($topic);	
 ?>
+</table>
